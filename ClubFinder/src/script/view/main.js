14ef300bfb52037d0e7bb81a5ce69864c9249a3e
manabil <mammarnabil@students.unnes.ac.id>
2022-11-16 23:47:44 +0700

    Search components FAILED (UNKNOWN ERROR)

--- ClubFinder/src/script/view/main.js
+++ ClubFinder/src/script/view/main.js
@@ -1,30 +1,34 @@
 'use strict';
+import '../components/search-bar.js';
 import DataSource from '../data/data-source.js';
 
 const main = () => {
-  const searchElement = document.querySelector('#searchElement');
-  const buttonSearchElement = document.querySelector('#searchButtonElement');
+  const searchElement = document.querySelector('search-bar');
   const clubListElement = document.querySelector('#clubList');
 
-  const onButtonSearchClicked = () => {
-    const dataSource = new DataSource(renderResult, fallbackResult);
-    dataSource.searchClub(searchElement.value);
+  const onButtonSearchClicked = async () => {
+    try {
+      const result = await DataSource.searchClub(searchElement.value);
+      renderResult(result);
+    } catch (message) {
+      fallbackResult(message);
+    }
   };
 
   const renderResult = (results) => {
     clubListElement.innerHTML = '';
-    results.forEach(function (club) {
-      const { name, fanArt, description } = club;
 
+    results.forEach((club) => {
+      const { name, fanArt, description } = club;
       const clubElement = document.createElement('div');
       clubElement.setAttribute('class', 'club');
-
       clubElement.innerHTML = `
         <img class="fan-art-club" src="${fanArt}" alt="Fan Art">
         <div class="club-info">
           <h2>${name}</h2>
           <p>${description}</p>
-        </div>`;
+        </div>
+      `;
       clubListElement.appendChild(clubElement);
     });
   };
@@ -34,7 +38,7 @@ const main = () => {
     clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`;
   };
 
-  buttonSearchElement.addEventListener('click', onButtonSearchClicked);
+  searchElement.clickEvent = onButtonSearchClicked;
 };
 
 export default main;
