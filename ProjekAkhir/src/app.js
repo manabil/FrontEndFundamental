c7a32d3a1d27f659bcff31bc5403eb077ccb9567
manabil <mammarnabil@students.unnes.ac.id>
2022-11-20 22:27:30 +0700

    Add counter feature

---
+++ ProjekAkhir/src/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import 'regenerator-runtime';
+import './style/style.css';
+import './script/view/sliding.js';
+import './script/view/counter.js';
+import $ from 'jquery';
+
+$(function () {
+  console.log('jos');
+});
